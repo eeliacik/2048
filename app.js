cb3600236506bbc75fd1285ab9d7d523591fd904
eeliacik <erhaneliacik@gmail.com>
2022-06-05 00:02:38 +0300

    creating operation arrays completed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   generateNumber();
   fillBoard();
 
+
   function fillSquaresArray() {
     for (let i = 0; i < 16; i++) {
       squares.push(0);
@@ -34,11 +35,48 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  // Move right
-  // function moveRight() {
-  //   const firstRow = [];
-  //   const secondRow = [];
-  //   for (let i = 0; i < 16; i++) {
-  //   }
-  // }
+  // Creating operaiton arrays on horizantal move
+  function horizontalMove() {
+    const firstRow = [];
+    const secondRow = [];
+    const thirdRow = [];
+    const fourthRow = [];
+    for (let i = 0; i < 16; i++) {
+      if (i < 4) {
+        firstRow.push(squares[i]);
+      } else if (3 < i < 8) {
+        secondRow.push(squares[i]);
+      } else if (7 < i < 12) {
+        thirdRow.push(squares[i]);
+      } else {
+        fourthRow.push(squares[i]);
+      }
+    }
+  };
+  
+  // Creating operaiton arrays on vertical move
+  function verticalMove() {
+    const firstColumn = [];
+    const secondColumn = [];
+    const thirdColumn = [];
+    const fourthColumn = [];
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        firstColumn.push(squares[i]);
+      } else if (i % 4 === 1) {
+        secondColumn.push(squares[i]);
+      } else if (i % 4 === 2) {
+        thirdColumn.push(squares[i]);
+      } else {
+        fourthColumn.push(squares[i]);
+      }
+    }
+  };
+
+  // Move up
+  function moveUp() {
+    
+  }
+
+
 });
